827aea311e22719ae3e9eb69747c9523c19cb96e
cristoferJaimez <jaimez07788@gmail.com>
2023-09-27 21:59:25 -0500

    Login Diseño

--- app/src/main/java/com/dev/apprx/MainActivity.kt
+++ app/src/main/java/com/dev/apprx/MainActivity.kt
@@ -11,7 +11,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.dev.apprx.ui.theme.AppRxTheme
-import com.dev.apprx.ui.theme.LoginScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
